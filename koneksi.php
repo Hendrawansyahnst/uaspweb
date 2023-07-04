b75f05008f5bc6e5556309efe176764103f7bf44
hendrawansyah <12150113914@students.uin-suska.ac.id>
2023-07-05 00:28:44 +0700

    upe

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,12 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:5173');
-header('Access-Control-Allow-Header: Content-Type');
 header('Access-Control-Allow-Method: GET, POST, OPTION, axios');
 
     function getConnection() {
         $host = 'localhost';
-        $dbname = 'uas';
-        $username = 'root';
-        $password = '';
+        $dbname = 'id20997322_nstbooknii';
+        $username = 'id20997322_nst';
+        $password = 'Kokbisalupasih!23';
 
         try {
             $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
